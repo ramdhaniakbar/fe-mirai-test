9bce83c437b4f08f65d4f23fb2cf3f085f82f866
Muhamad Ramdhani Akbar <ramdhaniakbarmuhamad@gmail.com>
2025-02-07 21:03:14 +0700

    feat: update homepage

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,26 +3,26 @@ import mirai from '../assets/images/mirai.png'
 
 const Navbar = () => {
   return (
-    <nav className="w-full bg-white/30 backdrop-blur-2xl flex gap-12 border-b border-[#8C8C8C]">
+    <nav className="fixed top-0 left-0 w-full bg-white/20 backdrop-blur-md flex gap-12 border-b border-[#8C8C8C] z-50">
       <div className="py-3 px-5 border-r border-[#8C8C8C]">
         <img src={mirai} alt="Mirai Technologies" className="w-30" />
       </div>
       <ul className="flex items-center space-x-10">
         <li>
-          <Link to={"/"}>Home</Link>
+          <Link to={"/"} className="text-primary">Home</Link>
         </li>
         <li>
-          <Link to={"/diary"}>List Diaries</Link>
+          <Link to={"/diary"} className="text-primary">List Diaries</Link>
         </li>
         <li>
-          <Link to={"/kodepos"}>Kodepos</Link>
+          <Link to={"/kodepos"} className="text-primary">Kodepos</Link>
         </li>
         <li>
-          <Link to={"/profile"}>Profile</Link>
+          <Link to={"/profile"} className="text-primary">Profile</Link>
         </li>
       </ul>
     </nav>
-  )
-}
+  );
+};
 
 export default Navbar
--- src/pages/homepage/HomePage.jsx
+++ src/pages/homepage/HomePage.jsx
@@ -3,17 +3,34 @@ import background from "../../assets/images/bg-home.jpg"
 const HomePage = () => {
   return (
     <div className="relative min-h-screen">
-      {/* Background Image Positioned on the Top-Right */}
       <div className="absolute top-0 right-0 -z-10">
         <img
           src={background}
           alt="Background Home"
-          className="w-[496px] h-[700px] object-cover rounded-bl-[56px]"
+          className="w-[496px] h-[750px] object-cover rounded-bl-[56px]"
         />
       </div>
 
-      {/* Content of HomePage */}
-      <h1 className="text-3xl font-bold relative z-10 p-6">HomePage</h1>
+      <div className="pt-44 pl-[160px] pr-[615px] flex flex-col gap-2">
+        <div className="flex items-center">
+          <h2 className="text-lg text-secondary font-medium uppercase">
+            — Inovasi, Efisiensi, dan Performa Maksimal
+          </h2>
+        </div>
+        <h1 className="text-4xl text-primary font-bold relative z-10 leading-12 mb-4">
+          Transformasi Digital untuk Masa Depan: Mewujudkan Inovasi, Efisiensi,
+          dan Keunggulan Teknologi
+        </h1>
+        <p className="text-gray mb-6">
+          Membangun solusi teknologi yang mendorong pertumbuhan dan efisiensi
+          bisnis. Kami adalah tim profesional di bidang pengembangan perangkat
+          lunak, dengan fokus pada aplikasi web, mobile, dan sistem backend yang
+          scalable. Dengan pengalaman luas di industri teknologi, kami membantu
+          bisnis dan individu menciptakan solusi digital yang inovatif, andal,
+          dan berkelanjutan.
+        </p>
+        <button className="bg-secondary text-white w-fit py-4 px-8 cursor-pointer">Selengkapnya</button>
+      </div>
     </div>
   )
 }
